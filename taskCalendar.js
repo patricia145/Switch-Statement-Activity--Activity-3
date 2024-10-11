61d5c99c6d6632ee0683ed289012b35f00a6f30e
patricia145 <patriciavaldez0517@gmail.com>
2024-10-11 22:33:35 +0800

    initial commit

--- taskCalendar.js
+++ taskCalendar.js
@@ -2,7 +2,7 @@ function scheduleCheck() {
     let day = prompt("Enter a day of the week: ");
     switch (day) {
         case "Monday":
-            alert("Your task for Monday is: Finish your assignments");
+            alert("Your task for Monday is: Do homework");
             break;
         case "Tuesday":
             alert("Your task for Tuesday is: Doing Household Chores");
@@ -14,13 +14,13 @@ function scheduleCheck() {
             alert("Your task for Thursday is: Exercise");
             break;
         case "Friday":
-            alert("Your task for Friday is: Sleep All Day");
+            alert("Your task for Friday is: Gala with Friends");
             break;
         case "Saturday":
-            alert("Your task for Saturday is: Doing Mukbang");
+            alert("Your task for Saturday is: Study");
             break;
         case "Sunday":
-            alert("Your task for Sunday is: Gala");
+            alert("Your task for Sunday is: Go to Church");
             break;
         default:
             alert("Please only enter a day of a week.");
